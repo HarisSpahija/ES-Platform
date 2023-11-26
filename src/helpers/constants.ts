e654e09e79a30800722ce2dc3fad7b8cd723f421
HarisSpahija <harisspahija@gmail.com>
2023-11-26 18:34:35 +0100

    Added constants

---
+++ src/helpers/constants.ts
@@ -0,0 +1,9 @@
+const kwhCostInCents = 180 // 0,18€
+const kwhCostInCentsPeak = 200 // 0,20€
+
+const startTimePeak = 7 // 7:00 AM
+const endTimePeak = 23 // 23:00 PM
+
+const meterInterValInMs = 1000 * 60 * 15 // 1 hour
+
+const daysThatHasPeak = [1, 2, 3, 4, 5] // Monday to Friday
